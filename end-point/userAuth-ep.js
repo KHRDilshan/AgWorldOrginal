df061a7255465e30ede62569aa82749c20f956a8
KHRDilshan <127587609+KHRDilshan@users.noreply.github.com>
2024-10-25 12:13:18 +0530

    Update userAuth-ep.js

--- end-point/userAuth-ep.js
+++ end-point/userAuth-ep.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
-// const db = require("../startup/database");
-// const asyncHandler = require("express-async-handler");
+const db = require("../startup/database");
+const asyncHandler = require("express-async-handler");
 // const {
 //   loginUserSchema,
 //   signupUserSchema,
