919f7b7ec6b439857c12298190e4f0c600bc4dfb
KHRDilshan <127587609+KHRDilshan@users.noreply.github.com>
2024-10-25 11:48:01 +0530

    Update userAutth.routes.js

--- routes/userAutth.routes.js
+++ routes/userAutth.routes.js
@@ -8,21 +8,21 @@ const {
   updateFirstLastName
 } = require("../Controllers/userAuth.controller");
 const auth = require("../Middlewares/auth.middleware");
-const userAuthEp = require("../end-point/userAuth-ep");
+// const userAuthEp = require("../end-point/userAuth-ep");
 const router = express.Router();
 
-// router.post("/user-register", userAuthEp.SignupUser);
+router.post("/user-register", userAuthEp.SignupUser);
 
 
-// router.post("/user-login", userAuthEp.loginUser);
+router.post("/user-login", userAuthEp.loginUser);
 
-// router.get("/user-profile", auth, userAuthEp.getProfileDetails);
+router.get("/user-profile", auth, userAuthEp.getProfileDetails);
 
-// router.put("/user-updatePhone", auth, userAuthEp.updatePhoneNumber);
+router.put("/user-updatePhone", auth, userAuthEp.updatePhoneNumber);
 
-// router.post("/user-register-checker", userAuthEp.signupChecker );
+router.post("/user-register-checker", userAuthEp.signupChecker );
 
-// router.put("/user-update-names", auth, userAuthEp.updateFirstLastName );
+router.put("/user-update-names", auth, userAuthEp.updateFirstLastName );
 
 
 module.exports = router;
