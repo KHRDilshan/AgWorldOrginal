b10adfd5a732388776bccb8d665a3817b7f08da3
KHRDilshan <127587609+KHRDilshan@users.noreply.github.com>
2024-10-25 13:48:59 +0530

    Update userAuth-validation.js

--- validations/userAuth-validation.js
+++ validations/userAuth-validation.js
@@ -19,8 +19,9 @@ exports.updatePhoneNumberSchema = Joi.object({
 });
 
 exports.signupCheckerSchema = Joi.object({
-   phoneNumber: Joi.string().optional().label('Phone Number'),
-});
+    phoneNumber: Joi.string().optional().label('Phone Number'),
+    NICnumber: Joi.string().optional().label('NIC Number'),
+  }).or('phoneNumber', 'NICnumber').label('Request Data');
 
   exports.updateFirstLastNameSchema = Joi.object({
     firstName: Joi.string().required().label('First Name'),
