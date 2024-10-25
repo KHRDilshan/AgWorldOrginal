7256ad7bff9e0832fe5a50e93cabd03e555660e8
KHRDilshan <127587609+KHRDilshan@users.noreply.github.com>
2024-10-25 10:21:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,34 +2,16 @@
 const express = require("express");
 const home = require("./routes/home");
 const newsRoutes = require("./routes/news");
-const cropRoutes = require("./routes/cropRoutes");
-const MarketPriceRoutes = require("./routes/marketPriceRoutes");
 
 
 // Middlewares
 const app = express();
 app.use(express.json());
 
-// Route imports
-const myCropRoutes = require("./routes/UserCrop.routes");
-app.use(process.env.AUTHOR, myCropRoutes);
-
-// const userRoutes = require("./routes/userAutth.routes");
-// app.use(process.env.AUTHOR, userRoutes);
-
-// const userFixedAssetsRoutes = require("./routes/fixedAsset.routes");
-// app.use(process.env.AUTHOR, userFixedAssetsRoutes);
-
-// const userCurrentAssetsRoutes = require("./routes/currentAssets.routes");
-// app.use(process.env.AUTHOR, userCurrentAssetsRoutes);
-
-// const publicforumRoutes = require("./routes/publicforum.routes");
-// app.use(process.env.AUTHOR, publicforumRoutes);
-
-// API routes
+// Routes
+app.use("/home", home);
 app.use("/api/news", newsRoutes);
-app.use("/api/crop", cropRoutes);
-// app.use("/api/market-price", MarketPriceRoutes);
+app.use("/api/news", newsRoutes);
 
 // connection
 const port = process.env.PORT || 9001;
