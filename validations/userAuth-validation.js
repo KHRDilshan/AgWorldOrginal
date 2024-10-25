50c66cdb60395b39fae3d73a9bd21479e3bdad31
KHRDilshan <127587609+KHRDilshan@users.noreply.github.com>
2024-10-25 16:26:11 +0530

    Update userAuth-validation.js

--- validations/userAuth-validation.js
+++ validations/userAuth-validation.js
@@ -1,56 +1,31 @@
-// const Joi = require('joi');
-
-// exports.loginUserSchema = Joi.object({
-//     phonenumber: Joi.string().required().label('Phone number'),
-// });
-
-
-// exports.signupUserSchema = Joi.object({
-//     firstName: Joi.string().min(2).max(50).required().label('First Name'),
-//     lastName: Joi.string().min(2).max(50).required().label('Last Name'),
-//     // phoneNumber: Joi.string().regex(/^[0-9]{10,15}$/).required().label('Phone Number'), // Should be a valid phone number format
-//     phoneNumber: Joi.number().integer().min(1000000000).max(999999999999999).required().label('Phone Number'), // Should be a valid number with 10 to 15 digits
-//     NICnumber: Joi.string().min(9).max(12).required().label('NIC Number') // Adjust to your country's NIC format
-// });
-
-// exports.updatePhoneNumberSchema = Joi.object({
-//     //newPhoneNumber: Joi.string().required().label('New Phone Number'), // New phone number must be provided
-//     newPhoneNumber: Joi.number().integer().min(1000000000).max(999999999999999).required().label('New Phone Number'),
-// });
-
-// exports.signupCheckerSchema = Joi.object({
-//     phoneNumber: Joi.string().optional().label('Phone Number'),
-//     NICnumber: Joi.string().optional().label('NIC Number'),
-//   });
-
-//   exports.updateFirstLastNameSchema = Joi.object({
-//     firstName: Joi.string().required().label('First Name'),
-//     lastName: Joi.string().required().label('Last Name')
-// });
-
 const Joi = require('joi');
 
 exports.loginUserSchema = Joi.object({
-  phonenumber: Joi.string().pattern(/^[0-9+]{10,15}$/).required().label('Phone number')
+    phonenumber: Joi.string().required().label('Phone number'),
 });
 
+
 exports.signupUserSchema = Joi.object({
-  firstName: Joi.string().min(2).max(50).required().label('First Name'),
-  lastName: Joi.string().min(2).max(50).required().label('Last Name'),
-  phoneNumber: Joi.string().pattern(/^[0-9+]{10,15}$/).required().label('Phone Number'),
-  NICnumber: Joi.string().min(9).max(12).required().label('NIC Number')
+    firstName: Joi.string().min(2).max(50).required().label('First Name'),
+    lastName: Joi.string().min(2).max(50).required().label('Last Name'),
+    // phoneNumber: Joi.string().regex(/^[0-9]{10,15}$/).required().label('Phone Number'), // Should be a valid phone number format
+    phoneNumber: Joi.number().integer().min(1000000000).max(999999999999999).required().label('Phone Number'), // Should be a valid number with 10 to 15 digits
+    NICnumber: Joi.string().min(9).max(12).required().label('NIC Number') // Adjust to your country's NIC format
 });
 
 exports.updatePhoneNumberSchema = Joi.object({
-  newPhoneNumber: Joi.string().pattern(/^[0-9+]{10,15}$/).required().label('New Phone Number')
+    //newPhoneNumber: Joi.string().required().label('New Phone Number'), // New phone number must be provided
+    newPhoneNumber: Joi.number().integer().min(1000000000).max(999999999999999).required().label('New Phone Number'),
 });
 
 exports.signupCheckerSchema = Joi.object({
-  phoneNumber: Joi.string().pattern(/^[0-9+]{10,15}$/).optional().label('Phone Number'),
-  NICnumber: Joi.string().min(9).max(12).optional().label('NIC Number')
-}).or('phoneNumber', 'NICnumber').label('Request Data');
+    phoneNumber: Joi.string().optional().label('Phone Number'),
+    NICnumber: Joi.string().optional().label('NIC Number'),
+  });
 
-exports.updateFirstLastNameSchema = Joi.object({
-  firstName: Joi.string().min(2).max(50).required().label('First Name'),
-  lastName: Joi.string().min(2).max(50).required().label('Last Name')
+  exports.updateFirstLastNameSchema = Joi.object({
+    firstName: Joi.string().required().label('First Name'),
+    lastName: Joi.string().required().label('Last Name')
 });
+
+
