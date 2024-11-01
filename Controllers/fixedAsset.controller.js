874f865ab985ab7e5e0cd2b931e33929a1ed9697
KHRDilshan <127587609+KHRDilshan@users.noreply.github.com>
2024-11-01 13:11:27 +0530

    Update fixedAsset.controller.js

--- Controllers/fixedAsset.controller.js
+++ Controllers/fixedAsset.controller.js
@@ -1,8 +1,6 @@
 const { response } = require('express');
 const db = require('../startup/database');
-const {
-addFixedAssetSchema
-} = require("../validations/fixedAssest-validation"); 
+
 
 // Helper function to format date
 const formatDate = (dateString) => {
@@ -13,10 +11,6 @@ const formatDate = (dateString) => {
 
 // Add a fixed asset
 exports.addFixedAsset = (req, res) => {
-      const { error } = addFixedAssetSchema(req.body);
-    if (error) {
-        return res.status(400).json({ message: 'Validation error', errors: error.details });
-    }
     const userId = req.user.id;
     const {
         category, ownership, type, floorArea, generalCondition, district,
